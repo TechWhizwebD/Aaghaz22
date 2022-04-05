752008514e03ae61d2b146c2426d1ff368cc160e
HardikBhanot <hardikbhanot123@gmail.com>
2022-04-05 12:24:54 +0530

    timeline edited

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 app.use(express.static(path.join(__dirname, 'static\\CSS')));
 app.use(express.static("static"));
-app.get("/", function(req, res) {
+app.get("/aaghaz", function(req, res) {
     res.sendFile(__dirname + "\\static\\index.html");
 
 });
@@ -35,6 +35,51 @@ app.get("/events/technical", function(req, res) {
 });
 app.get("/events/timeline", function(req, res) {
     res.sendFile(__dirname + "\\static\\timeline.html");
+});
+app.get("/kfwbdfwkjfbsdkkncljlnf",(req,res)=>{
+    res.redirect("https://instagram.com/tech_trail_6");
+});
+app.get("/kawbdfwkjfbsdkkncljlnf",(req,res)=>{
+    res.redirect("https://t.me/Techwhiz_bot");
+});
+app.get("/kbwbdfwkjfbsdkkncljlnf",(req,res)=>{
+    res.redirect("https://t.me/+otYAfPdcNalhOTRh");
+});
+app.get("/kcwbdfwkjfbsdkkncljlnf",(req,res)=>{
+    res.redirect();
+});
+app.get("/kdwbdfwkjfbsdkkncljlnf",(req,res)=>{
+    res.redirect();
+});
+app.get("/kewbdfwkjfbsdkkncljlnf",(req,res)=>{
+    res.redirect();
+});
+app.get("/kffwbdfwkjfbsdkkncljlnf",(req,res)=>{
+    res.redirect();
+});
+app.get("/kgwbdfwkjfbsdkkncljlnf",(req,res)=>{
+    res.redirect();
+});
+app.get("/khwbdfwkjfbsdkkncljlnf",(req,res)=>{
+    res.redirect();
+});
+app.get("/kiwbdfwkjfbsdkkncljlnf",(req,res)=>{
+    res.redirect();
+});
+app.get("/kjwbdfwkjfbsdkkncljlnf",(req,res)=>{
+    res.redirect();
+});
+app.get("/kkwbdfwkjfbsdkkncljlnf",(req,res)=>{
+    res.redirect();
+});
+app.get("/klwbdfwkjfbsdkkncljlnf",(req,res)=>{
+    res.redirect();
+});
+app.get("/kmwbdfwkjfbsdkkncljlnf",(req,res)=>{
+    res.redirect();
+});
+app.get("/knwbdfwkjfbsdkkncljlnf",(req,res)=>{
+    res.redirect();
 });
 
 app.listen(3000, function() {
